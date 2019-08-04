af57c930bdf47d4fbd1815cf326cd8a37a1429b7
wizardrepo <danielfrancislakey@gmail.com>
2019-08-05 00:12:02 +1000

    Updates

---
+++ precache-manifest.affc84f02e8e6c206318fb966d889fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af44796fefd8366d35f8e8d72c81ecc",
+    "url": "/React-Rolodex/index.html"
+  },
+  {
+    "revision": "800f77062a0760246fd5",
+    "url": "/React-Rolodex/static/css/main.feab0124.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React-Rolodex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "800f77062a0760246fd5",
+    "url": "/React-Rolodex/static/js/main.881aa4cd.chunk.js"
+  },
+  {
+    "revision": "703ca20d222e12f3b93a",
+    "url": "/React-Rolodex/static/js/runtime~main.9d4dfc7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
